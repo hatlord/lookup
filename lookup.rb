8f97a517bc9537dc999f384782ad04d7a0d19a83
richard.ackroyd <richard.ackroyd@nccgroup.com>
2021-07-20 14:07:55 +0100

    added gem

--- lookup.rb
+++ lookup.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 #lookup - List of hostnames to IP addresses in correct format.
 #./lookup.rb list_of_hostnames.txt
+require 'socket'
+
 hostnames = File.readlines(ARGV[0]).map(&:chomp &&:strip)
 
 
